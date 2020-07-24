27804b43232793ac7cbb94ee0ebcf2f4e8f5e669
srikanthpragada <srikanthpragada@gmail.com>
2020-07-24 08:59:14 +0530

    24-JULY

--- webdemo/demo/emp_views.py
+++ webdemo/demo/emp_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 import requests
 import sqlite3
+from .forms import EmployeeForm
 
 
 def add_employee(request):
@@ -24,6 +25,36 @@ def add_employee(request):
                 return render(request, 'add_employee.html', {'message' : message})
     else:
         return render(request, 'add_employee.html')
+
+
+def add_employee_with_form(request):
+    if request.method == 'GET':
+       f = EmployeeForm()
+       return render(request, 'add_employee_with_form.html', {'form': f})
+
+    # process data for POST request
+    f = EmployeeForm(request.POST)  # Bind form with data from request
+    if f.is_valid():
+        name = f.cleaned_data['name']
+        job = f.cleaned_data['job']
+        salary = f.cleaned_data['salary']
+        with sqlite3.connect(r"c:\classroom\june16\hr.db") as con:
+           cur = con.cursor()
+           try:
+              cur.execute("insert into employees(name,job,salary) values(?,?,?)",
+                      (name, job, salary))
+              con.commit()
+              message = f"Employee [{name}] was added successfully!"
+              f = EmployeeForm()  # Empty form
+              return render(request, 'add_employee_with_form.html',
+                                 {'message' : message, 'form' : f})
+           except Exception as ex:
+              print(ex)
+              message = "Sorry! Could not add employee due to error!"
+              return render(request, 'add_employee_with_form.html',
+                               {'message' : message,'form' : f})
+    else: # form is invalid
+        return render(request, 'add_employee_with_form.html',{'form': f})
 
 
 def list_employees(request):
--- webdemo/demo/urls.py
+++ webdemo/demo/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("countries/", views.list_countries),
     path("country/", views.get_country_info),
     path("addemployee/", emp_views.add_employee),
+    path("addemployee2/", emp_views.add_employee_with_form),
     path("employees/", emp_views.list_employees),
 ]
